c383d7b26868ebeb2c07248ea12f17e4147b0d78
Luke Rohde <lukerohde@gmail.com>
2016-05-18 17:10:22 +1000

    heroku and maybe rails4 doesn't like config.action_mailer.perform_caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "join_a_union_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+  # RAILS5??
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
