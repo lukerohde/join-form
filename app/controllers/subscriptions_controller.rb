39b58282f3af06699f0d787d661cf593d547a17b
Luke Rohde <lukerohde@gmail.com>
2016-07-14 10:19:38 +1000

    sending welcome message to member now - still an issue decerning welcomes from updates, a message may even be sent when the system finds a paying person

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -351,11 +351,11 @@ class SubscriptionsController < ApplicationController
     end
 
     def welcome
-      # I'm being really cautious here due to the complexity, but should be required with 'deliver_later' which would ordinarily crash in the background and send an exception
+      # I'm being cautious here due to the complexity, but should not be required with 'deliver_later' which would ordinarily crash in the background and send an exception
     
       begin
         template_id = @subscription.join_form.welcome_email_template_id
-        EmailTemplateMailer.merge(template_id, @subscription.id, 'lrohde@nuw.org.au').deliver_later if template_id.present?
+        EmailTemplateMailer.merge(template_id, @subscription.id, @subscription.person.email).deliver_later if template_id.present?
       rescue Exception => exception
         ExceptionNotifier.notify_exception(exception,
           :env => request.env, :data => {:message => "failed to send welcome email"})
