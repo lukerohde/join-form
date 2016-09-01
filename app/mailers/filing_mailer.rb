303e26de529eeab56a12409db50cc3867d781fa4
Luke Rohde <lukerohde@gmail.com>
2016-09-01 23:41:42 +1000

    fix from/to in sms description

--- app/mailers/filing_mailer.rb
+++ app/mailers/filing_mailer.rb
@@ -15,7 +15,7 @@ class FilingMailer < ApplicationMailer
 	def file_sms(msg, person_id = nil, tags = "")
 		if to = ENV['filing_email']
 			person = Person.find_by_id(person_id)
-			@subject = "DONE: #{tags} SMS from #{person.try(:display_name)} #{'(' + person.external_id + ')' if person}"
+			@subject = "DONE: #{tags} SMS to #{person.try(:display_name)} #{'(' + person.external_id + ')' if person}"
 			mail to: to, body: msg, subject: @subject
 		end
 	end
