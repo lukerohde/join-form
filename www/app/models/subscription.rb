d21169dc97f76209856ce8c43a2bc0af4940ca16
LUKE ROHDE <lukerohde@gmail.com>
2018-05-16 12:20:05 +1000

    allow zero charge forms for Adero class action

--- www/app/models/subscription.rb
+++ www/app/models/subscription.rb
@@ -6,20 +6,20 @@ class Subscription < ApplicationRecord
   belongs_to :join_form
   accepts_nested_attributes_for :person
   has_many :payments, autosave: true, dependent: :destroy
-  
+
   validates :person, :join_form, presence: true
   validate :subscription_must_be_complete, if: :address_saved?
   validate :address_must_be_complete, if: :contact_details_saved?
   validate :pay_method_must_be_complete, if: :subscription_saved?
-  
-  delegate :external_id, to: :person, allow_nil: true 
+
+  delegate :external_id, to: :person, allow_nil: true
   delegate :schema_data, to: :join_form, allow_nil: true
   scope :with_mobile, -> { joins(:person).where.not(people: { mobile: [nil, ''] })}
   scope :with_email, -> { joins(:person).where.not(Person.arel_table[:email].matches('%@unknown.com'))}
- 
+
   serialize :data, HashSerializer
 
-  encrypt_with_public_key :card_number, :ccv, :account_number, :bsb, 
+  encrypt_with_public_key :card_number, :ccv, :account_number, :bsb,
     :symmetric => :never,
     :base64 => true,
     :key_pair => :get_key_pair,
@@ -28,7 +28,7 @@ class Subscription < ApplicationRecord
   mount_uploader :signature_image, SignatureUploader
   before_save :generate_signature_image
   before_save :set_data_if_blank
-  before_save :set_completed_at 
+  before_save :set_completed_at
 
   def set_completed_at
     # called after validation
@@ -43,7 +43,7 @@ class Subscription < ApplicationRecord
   end
 
   def step
-    return :thanks if errors.count == 0 && self.completed_at.present? && pay_method_saved? && subscription_saved? && (address_saved? || !address_required?) && contact_details_saved? 
+    return :thanks if errors.count == 0 && self.completed_at.present? && pay_method_saved? && subscription_saved? && (address_saved? || !address_required?) && contact_details_saved?
     return :pay_method if subscription_saved? && (address_saved? || !address_required?) && contact_details_saved?
     return :subscription if (address_saved? || !address_required?)  && contact_details_saved?
     return :address if contact_details_saved?
@@ -53,19 +53,19 @@ class Subscription < ApplicationRecord
   def set_data_if_blank
     self.data = {} if data.blank? # set a default, when blank, because of a possible AR/jsonb bug
   end
-  
+
   def get_key_pair
     self.join_form.union.key_pair
   end
 
   before_validation :set_token, on: [:create]
-  
+
   def set_token
     self.token = SecureRandom.urlsafe_base64(16) # equivalent of 128 bit key
   end
 
   def address_saved?
-    person.present? && person.address1_was.present? && person.suburb_was.present? && person.state_was.present? && person.postcode_was.present? 
+    person.present? && person.address1_was.present? && person.suburb_was.present? && person.state_was.present? && person.postcode_was.present?
   end
 
   def address_present?
@@ -73,12 +73,12 @@ class Subscription < ApplicationRecord
   end
 
   def contact_details_saved?
-    (person.present? && person.email_was.present? && !Person.temporary_email?(person.email_was) && person.first_name_was.present? && !Person.temporary_first_name?(person.first_name_was)) 
+    (person.present? && person.email_was.present? && !Person.temporary_email?(person.email_was) && person.first_name_was.present? && !Person.temporary_first_name?(person.first_name_was))
   end
 
   def subscription_saved?
     custom_columns_saved = true
-    (self.schema_data[:columns]||[]).each do |column| 
+    (self.schema_data[:columns]||[]).each do |column|
       custom_columns_saved = false if (data_was||{})[column].blank?
     end
 
@@ -104,7 +104,7 @@ class Subscription < ApplicationRecord
   end
 
   def set_country_code(location)
-    self.country_code ||= location 
+    self.country_code ||= location
     if self.country_code == nil || ["", "RD"].include?(self.country_code)
      self.country_code = ENV['ADDRESS_REQUIRED_COUNTRY_CODES'].split(',').first # use first as default
     end
@@ -112,12 +112,12 @@ class Subscription < ApplicationRecord
 
   def subscription_must_be_complete
     return if @skip_validation
-      
+
     errors.add(:plan,I18n.translate("subscriptions.errors.not_blank")) if plan.blank?
     errors.add(:frequency,I18n.translate("subscriptions.errors.not_blank")) if frequency.blank?
-    
+
     # validate presence of custom columns
-    (self.schema_data[:columns]||[]).each do |column| 
+    (self.schema_data[:columns]||[]).each do |column|
       errors.add(column,I18n.translate("subscriptions.errors.not_blank")) if data[column].blank?
     end
   end
@@ -129,7 +129,7 @@ class Subscription < ApplicationRecord
 
   def address_must_be_complete
     return if @skip_validation
-    return unless address_required? 
+    return unless address_required?
 
     if person
       person.errors.add(:address1,I18n.translate("subscriptions.errors.not_blank")) unless person.address1.present?
@@ -139,12 +139,12 @@ class Subscription < ApplicationRecord
       errors.add(:base,I18n.translate("subscriptions.errors.complete_address")) unless person.address_valid?
     end
   end
-  
+
   def pay_method_saved?
     #errors.empty? && (stripe_token_was.present? || (bsb_was.present? && account_number_was.present?) || pay_method == "-") || @skip_validation
     # TODO why am I somethings using _was, prevent welcome message being sent when matching an existing person
     # When a signature is already saved and I'm OOP
-    (errors.empty? && 
+    (errors.empty? &&
       (
         (self.pay_method == "AB" && self.join_form.direct_debit_on && has_existing_pay_method?) ||
         (self.pay_method == "CC" && self.join_form.credit_card_on && has_existing_pay_method?) ||
@@ -179,7 +179,7 @@ class Subscription < ApplicationRecord
   def pay_method=(value)
     write_attribute(:pay_method, value)
 
-    # A subscription can only be completed if a pay_method is written 
+    # A subscription can only be completed if a pay_method is written
     # Essentially confirming an existing pay_method when one already exists
     self.completed_at = Time.now
     self.pending = false
@@ -192,8 +192,8 @@ class Subscription < ApplicationRecord
       def record_timestamps; false; end
     end
 
-    begin 
-      self.save! 
+    begin
+      self.save!
     ensure
       class << self
        remove_method :record_timestamps
@@ -208,7 +208,7 @@ class Subscription < ApplicationRecord
     result
   end
 
-  def discount 
+  def discount
     dues = (self.payments.sum(:amount)||0)
     dues > establishment_fee ? establishment_fee : dues
   end
@@ -225,7 +225,7 @@ class Subscription < ApplicationRecord
     result = self.join_form.fee(self.frequency)
     result = self.join_form.fee(self.join_form.max_frequency) if (result||0) == 0 # This is probably stupid - defaults the user to the minimum amount chargeable if they have a different frequency
     # TODO add javascript to update the page, if the user changes the frequency when on the payment method step
-    result
+    result || 0
   end
 
   def first_payment
@@ -235,16 +235,16 @@ class Subscription < ApplicationRecord
       self.total
     end
   end
-  
+
   def update_with_payment(params, union)
     assign_attributes(params)
-    
+
     if valid?
       customer = Stripe::Customer.create({description: person.email, card: stripe_token} , {stripe_account: union.stripe_user_id})
       person.stripe_token = customer.id
-      
+
       stripe_amount = (self.first_payment * 100).round(0).to_i
-       
+
       if stripe_amount > 0
         charge = Stripe::Charge.create({amount: stripe_amount, currency: 'AUD', description: join_form.description, customer: person.stripe_token}, {stripe_account: union.stripe_user_id})
         self.payments << Payment.new(date: Date.today, amount: (stripe_amount / 100.0).round(2), person_id: self.person.id)
@@ -268,7 +268,7 @@ class Subscription < ApplicationRecord
 
   def update_from_end_point(payload)
     @skip_validation = true
-      
+
     subscription_payload = payload.except(:person_attributes, :payments_attributes)
     person_payload = payload[:person_attributes]
     payments_payload = payload[:payments_attributes]
@@ -301,19 +301,19 @@ class Subscription < ApplicationRecord
         self.payments.build(payment_payload)
       end
     end
-      
+
     save!
     @skip_validation = false
   end
 
-  def generate_signature_image 
+  def generate_signature_image
     if self.signature_vector.present? && self.signature_vector != self.signature_vector_was
       instructions = JSON.parse(signature_vector).map { |h| "line #{h['mx'].to_i},#{h['my'].to_i} #{h['lx'].to_i},#{h['ly'].to_i}" } * ' '
       tempfile = Tempfile.new(["signature", '.png'])
       Open3.popen3("convert -size 298x98 xc:transparent -stroke blue -draw @- #{tempfile.path}") do |input, output, error|
         input.puts instructions
       end
-      self.signature_date = Date.today 
+      self.signature_date = Date.today
       self.signature_image = File.open(tempfile)
     end
   end
